4683e3a3191977619457b11bb2118a1fbac74e6b
ksolomon <ksolomon@utexas.edu>
2025-04-24 03:21:40 -0700

    better files

--- web-client/src/api.ts
+++ web-client/src/api.ts
@@ -2,7 +2,7 @@ const BASE_URL = "http://10.224.174.3:34199"; // Your backend URL
 
 export async function uploadFileToContext(...files: File[]) {
   const formData = new FormData();
-  files.forEach(file => formData.append("file", file));
+  files.forEach(file => formData.append("files", file));
 
   const response = await fetch("/upload", {
     method: "POST",
